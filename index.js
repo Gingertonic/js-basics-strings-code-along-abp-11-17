e59a362fe0b24a8f149377c131cd60fdc51fa171
Beth Schofield <bethmschofield@gmail.com>
2017-11-10 19:24:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 let currentUser = 'Beti Leti';
 // const welcomeMessage = "Welcome to Flatbook, " + currentUser +"!";
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+const welcomeMessage = `Welcome to 
+Flatbook, ${currentUser}`;
